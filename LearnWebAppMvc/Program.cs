8c4997a5a3128d21ba092c9780ddef5a83e203c5
xiongyizhou <xiongyizhou@powerpms.com>
2023-02-16 11:05:56 +0800

    mvc的例子，学完了

--- LearnWebAppMvc/Program.cs
+++ LearnWebAppMvc/Program.cs
@@ -1,9 +1,33 @@
+using LearnWebAppMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
+}
+else
+{
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
+}
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+// init data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ LearnWebAppMvc/readme.md
@@ -0,0 +1,5 @@
+﻿
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 6.0.13
+dotnet add package Microsoft.EntityFrameworkCore.SQLite --version 6.0.13
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version 6.0.8
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 6.0.13
